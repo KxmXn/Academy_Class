27588712dfb72cd4d69324d4e1afe20dc4796647
강 기 민 <gimingng2311@naver.com>
2026-06-19 03:56:34 +0900

    Add files via upload

---
+++ jwt/src/main/java/com/example/demo/jwt/JWTUtil.java
@@ -0,0 +1,59 @@
+package com.example.demo.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+
+@Component
+public class JWTUtil {
+
+    private SecretKey secretKey;
+
+    // application.properties 
+    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
+        // value 값을 가져와서 Jwts.SIG.HS256.key() -> 객체 키(SecretKey)를 생성
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), 
+        		Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    // 검증용
+    public String getUsername(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build()
+        		.parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    // 검증용
+    public String getRole(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build()
+        		.parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    // 검증용
+    public Boolean isExpired(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build()
+        		.parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    // 생성
+    public String createJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
---
+++ jwt/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
+		
+	// 추가 username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
+    UserEntity findByUsername(String username);
+
+	
+}
